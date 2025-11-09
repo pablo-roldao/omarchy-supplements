00bee723a08bde9f40ad474b667f0d7b678ff88e
Pablo Roldão <dev@pabloroldao.com.br>
2025-11-09 12:03:15 -0300

    fix: remove typora desktop entry

--- fix-and-create-desktop-apps.sh
+++ fix-and-create-desktop-apps.sh
@@ -3,6 +3,7 @@
 rm ~/.local/share/applications/GitHub.desktop
 rm ~/.local/share/applications/WhatsApp.desktop
 rm ~/.local/share/applications/YouTube.desktop
+rm ~/.local/share/applications/typora.desktop
 
 cp ./applications/icons/*.png ~/.local/share/applications/icons/
 cp ./applications/*.desktop ~/.local/share/applications/
